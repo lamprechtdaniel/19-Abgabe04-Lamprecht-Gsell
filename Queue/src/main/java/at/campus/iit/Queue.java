5cca4fda74bab1874bda30815f05eb3cdd75599c
Mathias Gsell <mathias.gsell@edu.campus02.at>
2019-06-04 22:47:28 +0200

    javadoc comments modified

--- Queue/src/main/java/at/campus/iit/Queue.java
+++ Queue/src/main/java/at/campus/iit/Queue.java
@@ -1,15 +1,15 @@
 package at.campus.iit;
 
-public interface Queue {
+/**
+ * The interface Queue consists of all required methods which are necessary for the data structure Queue.
+ *
+ * @author	Mathias Gsell
+ * @author  Daniel Lamprecht
+ * @version 1.0
+ * @since	04.06.2019
+ */
 
-	/**
-	 * The interface Queue consists of all required methods which are necessary for the data structure Queue.
-	 *
-	 * @author	Mathias Gsell
-	 * @author  Daniel Lamprecht
-	 * @version 1.0
-	 * @since	04.06.2019
-	 */
+public interface Queue {
 	
 	/**
 	 *  Adds the element obj to the queue. 
